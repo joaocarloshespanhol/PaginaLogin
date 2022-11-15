d29f23709dee5e75b563ff9ce92ac65dc6a078c0
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2022-11-15 01:55:55 -0300

    Pagina de Login

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform} from 'react-native';
 
 export default function App() {
+
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function entrar() {
+    alert('AGUARDANDO...')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <KeyboardAvoidingView
+    behavior={Platform.OS == 'ios' ? "padding" : "height"} style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+
+      <Image style ={{width:180, height:180}} source={require('./assets/telainicio2.png')} />
+
+      <TextInput placeholder="Usuario" style={styles.textInput} onChangeText={text=>setUsuario(text)} />
+      <TextInput secureTextEntry={true} placeholder="Senha" style={styles.textInput} onChangeText={text=>setSenha(text)} />
+
+      <TouchableOpacity style={styles.botaoEntrar} onPress={()=>entrar()}>
+        <Text style={{color:'white'}}>ENTRAR</Text>
+      </TouchableOpacity>
+
+    </KeyboardAvoidingView>
   );
 }
 
@@ -16,5 +35,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:15
   },
+  textInput:{
+    width:'100%',
+    height:40,
+    backgroundColor:'#DCDCDC',
+    borderRadius:15,
+    paddingLeft:10,
+    marginBottom:10
+  },
+  botaoEntrar:{
+    width:'100%',
+    height:40,
+    backgroundColor:'#708090',
+    borderRadius:20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
